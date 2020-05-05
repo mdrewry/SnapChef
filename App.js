0abfdad1e420f65930178811326150acd37fc1d1
Rohan <rohan.samanta@hotmail.com>
2020-05-04 23:14:27 -0400

    made imagepicker a component

--- App.js
+++ App.js
@@ -1,44 +1,22 @@
-import React, {useState,useEffect} from 'react';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Button, Image } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
-import Constants from 'expo-constants';
-import * as Permissions from 'expo-permissions';
+import ImagePicker from "./ImagePicker"
+import { registerRootComponent } from 'expo';
 
-export default function ImagePickerExample() {
-  const [image,setImage] = useState(null);
-  
-  useEffect(()=>{getPermissionAsync();},[]);
-  async function getPermissionAsync(){
-    if (Constants.platform.ios) {
-      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
-      if (status !== 'granted') {
-        alert('Sorry, we need camera roll permissions to make this work!');
-      }
-    }
-  };
-  async function pickImage(){
-    try {
-      let result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.All,
-        allowsEditing: true,
-        aspect: [4, 3],
-        quality: 1,
-      });
-      if (!result.cancelled) {
-        setImage(result.uri)
-      }
-
-      console.log(result);
-    } catch (E) {
-      console.log(E);
-    }
-  };
+export default function App() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={styles.container}>
       <Text>Hello Rohan LOL</Text>
-      <Button title="Pick an image from camera roll" onPress={pickImage} />
-      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+      <ImagePicker/>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
