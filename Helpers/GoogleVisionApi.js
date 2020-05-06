9f0d2522ce3e446cc9e7f7be47ada3122aca978f
nelsonreina <34137044+nelsonreina@users.noreply.github.com>
2020-05-06 18:38:04 -0400

    Add files via upload

---
+++ Helpers/GoogleVisionApi.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { TextInput } from "react-native-paper";
+
+function GoogleVisionAPI() {
+  return (
+    <View>
+      <TextInput></TextInput>
+    </View>
+  );
+}
+
+export default GoogleVisionAPI;
