d1073c66c7ee7f64b84029f663c4ab53bb819157
Mark Drewry <mdrewry0@gmail.com>
2020-05-08 17:25:46 -0400

    implemented edemam api and made list

--- recipeListPage.js
+++ recipeListPage.js
@@ -1,11 +1,58 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { TextInput } from "react-native-paper";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { TextInput, List, Button } from "react-native-paper";
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  searchWrapper: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+    marginTop: 50,
+    padding: 5,
+    justifyContent: "space-between",
+  },
+  filter: {
+    width: "70%",
+  },
+});
 
-function recipeListPage() {
+function recipeListPage(searchInput) {
+  const [recipes, setRecipes] = useState([]);
+  const [searchBar, setSearchBar] = useState(searchInput);
+  async function getRecipes() {
+    await fetch(
+      `https://api.edamam.com/search?q=${searchBar}&app_id=${config.recipe.appID}&app_key=${config.recipe.key}`
+    )
+      .then((response) => {
+        response.json().then((data) => setRecipes(data.hits));
+      })
+      .catch((error) => console.log(error.status));
+  }
   return (
-    <View>
-      <TextInput></TextInput>
+    <View style={styles.container}>
+      <View style={styles.searchWrapper}>
+        <TextInput
+          style={styles.filter}
+          value={searchBar}
+          onChangeText={(info) => setSearchBar(info)}
+        />
+        <Button onPress={(e) => getRecipes(e)}>Search</Button>
+      </View>
+      {recipes.map((r) => (
+        <View key={r.recipe.label} style={{ flexDirection: "row" }}>
+          <List.Item
+            style={{ width: "80%" }}
+            title={r.recipe.label}
+          ></List.Item>
+          <Image
+            style={{ width: "20%" }}
+            source={{ uri: r.recipe.image }}
+          ></Image>
+        </View>
+      ))}
     </View>
   );
 }
