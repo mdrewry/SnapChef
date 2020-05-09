012478635854a2476dc47b46ee5bd7e17b690f51
Mark Drewry <mdrewry0@gmail.com>
2020-05-08 23:54:24 -0400

    added css to list objects, implemented search

--- recipeListPage.js
+++ recipeListPage.js
@@ -1,17 +1,18 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
-import { TextInput, List, Button } from "react-native-paper";
+import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
+import { TextInput, List, Button, Card, Title } from "react-native-paper";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
+    backgroundColor: "#393e46",
   },
   searchWrapper: {
     flexDirection: "row",
     alignItems: "center",
+    paddingTop: 50,
+    padding: 10,
     width: "100%",
-    marginTop: 50,
-    padding: 5,
     justifyContent: "space-between",
   },
   filter: {
@@ -19,9 +20,11 @@ const styles = StyleSheet.create({
   },
 });
 
-function recipeListPage(searchInput) {
+function recipeListPage(props) {
   const [recipes, setRecipes] = useState([]);
-  const [searchBar, setSearchBar] = useState(searchInput);
+  const googleInfo = "";
+  const [searchBar, setSearchBar] = useState(googleInfo);
+  const config = {};
   async function getRecipes() {
     await fetch(
       `https://api.edamam.com/search?q=${searchBar}&app_id=${config.recipe.appID}&app_key=${config.recipe.key}`
@@ -41,18 +44,48 @@ function recipeListPage(searchInput) {
         />
         <Button onPress={(e) => getRecipes(e)}>Search</Button>
       </View>
-      {recipes.map((r) => (
-        <View key={r.recipe.label} style={{ flexDirection: "row" }}>
-          <List.Item
-            style={{ width: "80%" }}
-            title={r.recipe.label}
-          ></List.Item>
-          <Image
-            style={{ width: "20%" }}
-            source={{ uri: r.recipe.image }}
-          ></Image>
-        </View>
-      ))}
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {recipes.map((r) => (
+          <View
+            style={{
+              padding: 5,
+              flexDirection: "row",
+              margin: 10,
+              borderRadius: 5,
+              backgroundColor: "#222831",
+            }}
+            key={r.recipe.label}
+          >
+            <Image
+              style={{ width: 200, height: 150, borderRadius: 5 }}
+              source={{ uri: r.recipe.image }}
+            />
+            <View
+              style={{
+                marginLeft: 5,
+                width: 100,
+                height: 150,
+                alignItems: "center",
+                borderRadius: 5,
+                backgroundColor: "#FFFFFF",
+              }}
+            >
+              <Text style={{ fontSize: 10 }}>{r.recipe.label}</Text>
+              <Text style={{ fontSize: 8 }}>
+                {parseInt(r.recipe.calories)} calories
+              </Text>
+              <Text style={{ fontSize: 8, textDecoration: "underline" }}>
+                Ingredients
+              </Text>
+              {r.recipe.ingredients.map((ingredient) => (
+                <Text key={ingredient.text} style={{ fontSize: 5 }}>
+                  {ingredient.text}
+                </Text>
+              ))}
+            </View>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
